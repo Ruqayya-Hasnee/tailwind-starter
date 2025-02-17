2cd73001664a6cdc080da41d9893c20a92823a10
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2025-02-17 14:08:32 +0500

    using image component of nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-6xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex">
+      <div>
+        <Image src="/next.svg" width={100} height={100} alt="Next Logo" />
+      </div>
+      <div>
+        <h1>Welcome to NorthBy</h1>
+        <h2>A premium in sight and sound</h2>
+        <button>Learn More</button>
+      </div>
     </div>
   );
 }
